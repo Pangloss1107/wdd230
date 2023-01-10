c51c8221ecd5db1cdb4efd656ea805d84616a367
Pangloss1107 <pablonsantos1107@gmail.com>
2023-01-10 17:18:13 -0300

    updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -24,3 +24,21 @@ let minuteModif = oLastModif.getMinutes()
 let secondsModif = oLastModif.getSeconds()
 let completeMod = `Last Modification: ${dayModif}/${monthModif}/${yearModif} ${hourModif}:${minuteModif}:${secondsModif}`
 document.getElementById("modified").textContent = completeMod;
+
+
+// Dark Mode
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("dark_mode")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.innerHTML = '<span class="material-symbols-outlined">brightness_5</span>'
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.innerHTML = '<span class="material-symbols-outlined">dark_mode</span>'
+	}
+});
