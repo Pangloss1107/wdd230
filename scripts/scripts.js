8f6025fc5d244d22ca8945a9483656ec910d88ac
Pangloss1107 <pablonsantos1107@gmail.com>
2023-01-18 02:50:41 -0300

    updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -42,3 +42,23 @@ modeButton.addEventListener("click", () => {
 		modeButton.innerHTML = '<span class="material-symbols-outlined">dark_mode</span>'
 	}
 });
+
+
+// Obtain the number of visits
+
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
